81bb30c1bb9b3ea96aaa21069ff57628bd9f3726
dansan-dsn <ddryn970@gmail.com>
2024-06-06 11:43:40 +0300

    client side prt2

--- routes/login.route.js
+++ routes/login.route.js
@@ -2,14 +2,13 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const bcrypt = require('bcrypt')
-const nodemailer = require('nodemailer')
 const collection = require('../models/login.model')
 
 router.get('/login', (req, res) => {
 	res.render('partials/login', {error: null})
 })
       .get('/signup', (req, res) => {
-		res.render('partials/signup')
+		res.render('partials/signup', {err: null})
 	})
 
 	  .post('/signup', async (req, res) => {
@@ -22,38 +21,14 @@ router.get('/login', (req, res) => {
 
 			const c_password = req.body.comfirm_password
 
-            // const emailVerificationCode = generateRandomCode();
-
-			// const transporter = nodemailer.createTransport({
-			// 	service: 'gmail',
-			// 	auth: {
-			// 		user: data.email,
-			// 		pass: data.password
-			// 	}
-			// })
-
-			// const mailOptions = {
-			// 	from: 'Article',
-			// 	to: data.email,
-			// 	subject: 'Email verification code',
-			// 	text: `Your email verification code is: ${emailVerificationCode}`
-			// }
-
-			// transporter.sendMail(mailOptions, (error, info) => {
-			// 	if(error) {
-			// 		return res.status(500).send({message: 'error sending message'})
-			// 	}
-			// })
-
-
 			// check if user already exits in the database
 			const existingUser = await collection.findOne({email: data.email})
 			if(existingUser) {
-				res.render('partials/signup', {msg: 'Email has an account already.'})
+				res.render('partials/signup', {err: 'User already exits. Please choose a different email.'})
 			} else {
 
 				if(data.password != c_password) {
-					return res.status(400).render('partials/signup', {msg1: 'Password do not match'});
+					return res.status(400).render('partials/signup' ,{err: 'Password do not match'});
 				} else {
                    // hash the password using bcrypt
 				const saltRounds = 10;
@@ -63,10 +38,11 @@ router.get('/login', (req, res) => {
 
 				const userdata = await collection.insertMany(data)
 				console.log(userdata)
-				res.render('partials/login')
+				res.render('partials/login', {error: null})
 				}
 				 
 			}
+	
 	
 		} catch (error) {
 			res.status(500).json({message: error.message})
@@ -76,17 +52,18 @@ router.get('/login', (req, res) => {
 	  .post('/login', async (req, res) => {
 		try {
 			const check = await collection.findOne({email: req.body.email})
-			if(!check) {
-				res.redirect('/api/user/login',{err: 'User name cannot be found'})
-			}
-
-			// compare hashed password from the database with plain text
+			if(check) {
+				// compare hashed password from the database with plain text
 			const isPasswordMatch = await bcrypt.compare(req.body.password, check.password)
 			if(isPasswordMatch) {
 				res.status(200).render('home')
 			} else {
-				res.render('partials/login', { error: 'Wrong password. Please try again.'})
+				res.render('partials/login', { error: 'Wrong password. Please try again'})
 			}
+			}else {
+				res.render('partials/login' ,{ error: 'User name cannot be found'})
+			}
+			
 		} catch (error) {
 			res.status(500).json({message: error.message})
 		}
