041dc5c6fffc95a834be51faafcbd56e6c293772
dansan-dsn <ddryn970@gmail.com>
2024-06-19 16:42:28 +0300

    Notif.. as read

--- routes/like.route.js
+++ routes/like.route.js
@@ -4,13 +4,13 @@ const Notification = require('../models/notification')
 const Article = require('../models/article.model')
 const Like = require('../models/like')
 
-route.post('/create_likes', async (req, res) => {
+route.post('/create_likes/:userId/:articleId', async (req, res) => {
     try {
-        const { userId, articleId } = req.body
-        // const like = await Like.create({
-        //     article: articleId,
-        //     author: userId
-        // })
+        const { userId, articleId } = req.params
+        const like = await Like.create({
+            user: userId,
+            article: articleId
+        })
 
         const article = await Article.findById(articleId).populate('author')  // we are filling the author with id from the user model
 
@@ -24,6 +24,26 @@ route.post('/create_likes', async (req, res) => {
         res.status(500).json({error: error.message})
     }
 })
+
+    .get('/content-likes', async (req, res) => {
+        try {
+            const likes = await Like.findOne({})
+            res.status(200).json(likes)
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
+    })
+
+    .delete('/unlike/:id', async (req, res) => {
+        try {
+            const { id } = req.params
+            const unlike = await Like.findByIdAndDelete(id)
+            if(!unlike) return res.status(404).json({message: 'unliked article not found'})
+            res.status(200).json({message: 'article unliked'})
+        } catch (error) {
+            res.status(500).json({error: error.message})
+        }
+    })
 
 module.exports = route
 
--- routes/notifications.js
+++ routes/notifications.js
@@ -1,25 +1,60 @@
-const express = require('express')
-const route = express.Router()
-const Notification = require('../models/notification') 
+const express = require("express");
+const route = express.Router();
+const Notification = require("../models/notification");
 
-route.post('/create', async (req, res) => {
-     try {
+route
+  .put("/:id/read", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const notification = await Notification.findByIdAndUpdate(
+        id,
+        { read: true },
+        { new: true }
+      );
       
+      if (!notification) {
+        return res.status(404).json({ message: "Notification not found" });
+      }
+      res.status(200).json(notification);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  })
 
-     } catch (error) {
-        res.status(500).json({error: error.message})
-     }
-})
+  .get("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const notification = await Notification.findById(id);
+      if (!notification)
+        return res.status(404).json({ message: "Notifications not found" });
 
-      .get('/notification/:userId', async (req, res) => {
-         try {
-            const notifications = await Notification.find({
-               user: req.params.userId
-            })
-            res.status(200).json(notifications)
-         } catch (error) {
-            res.status(500).json({error: error.message})
-         }
-      })
+      res.status(200).json(notification);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  })
 
-module.exports = route
+  .get("/allNotificaions", async (req, res) => {
+    try {
+      const notifications = await Notification.find({});
+      if (!notifications)
+        return res.status(404).json({ message: "Notifications not found" });
+      res.status(200).json(notifications);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  })
+
+  .delete("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const notification = await Notification.findByIdAndDelete(id);
+      if (!notification)
+        return res.status(404).json({ message: "Notifications not found" });
+      res.status(200).json({ message: "Notifications successfully deleted" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+module.exports = route;
